594b7b1fe94a8c807d229a133b3beb8766b0b236
pitagoras3 <szymon.marcinkiewicz@gmail.com>
2018-04-29 03:36:08 +0200

    Changed project name.

---
+++ lab6/src/university/Student.java
@@ -0,0 +1,43 @@
+package university;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Student {
+
+    private String name;
+    private String surname;
+    private Gender gender;
+    private LocalDate dateOfBirth;
+    private String id;
+    private Department department;
+
+    public Student(String name, String surname, Gender gender, LocalDate dateOfBirth) {
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.id = generateId();
+    }
+
+    private String generateId(){
+        StringBuilder idGeneratorBuilder = new StringBuilder();
+        idGeneratorBuilder.append(gender.getGenderId());
+        idGeneratorBuilder.append(dateOfBirth.format(DateTimeFormatter.ofPattern("YYMMDD")));
+        idGeneratorBuilder.append(Math.abs((name + surname).hashCode()));
+        return idGeneratorBuilder.toString().substring(0, 12);
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%20s, %20s %-30s %10s", id, name, surname, dateOfBirth);
+    }
+}
